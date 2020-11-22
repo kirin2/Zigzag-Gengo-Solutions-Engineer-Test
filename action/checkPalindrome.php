94323eeab96139c2b89315958252f460f5606c8c
edgar <getzwebph@gmail.com>
2020-11-22 11:45:49 +0800

    use created function getMinCutPal and return it

--- action/checkPalindrome.php
+++ action/checkPalindrome.php
@@ -1,8 +1,8 @@
 <?php
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
 
 require_once "../class/Palindrome.php";
 
@@ -15,12 +15,16 @@ $bLvl1 = Palindrome::isPalindrome($sPalString);
 $slvl1Res = $bLvl1 == true ? "TRUE // its's written the same forward and backward" : 
                              "FALSE // not written the same forward and backward" ;
 
-$bLvl2 = Palindrome::getLongestPal($sPalString);
+$bLvl2Res = Palindrome::getLongestPal($sPalString);
+
+
+$bLvl3Res = Palindrome::getMinCutPal($sPalString);
 
 
 $aRet  = array(
     "lvl1" => $slvl1Res,
-    "lvl2" => $bLvl2
+    "lvl2" => $bLvl2Res ,
+    "lvl3" => $bLvl3Res
 );
 
 
